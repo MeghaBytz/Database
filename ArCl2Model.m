4b818de3f67ff521095128ba27fc2a6c0177f085
MeghaBytz <meghali.chopra@gmail.com>
2016-06-21 14:24:22 -0500

    database with arcl2 model

---
+++ ArCl2Model.m
@@ -0,0 +1,112 @@
+clear all
+close all
+
+%Reaction set from "Applicability of self-consistent global model for
+%characterization of inductively coupled Cl2 plasma" Efremov et al
+
+% 
+stoichiometricMatrix = [-1	0	1	0	0	-1;
+-1	0	0	1	1	-1;
+-1	1	0	0	1	-1;
+-1	2	0	0	0	-1;
+0	-1	0	1	0	0;
+0	1	0	-1	-1	0;
+0	1	-1	0	-1	0;
+0	1	0	0	-1	-1]
+
+
+
+
+% stoichiometricMatrix = [-1	0	0	1	0	0	0	-1;
+% 0	-1	0	0	1	0	0	-1;
+% -1	0	0	0	1	0	1	-1;
+% 0	0	-1	0	0	1	0	-1;
+% -1	1	0	0	0	0	1	-1;
+% 0	1	0	0	0	0	-1	-1;
+% -1	2	0	0	0	0	0	-1;
+% 0	3	0	-1	0	0	-1	0;
+% 0	2	0	-1	0	0	-1	0;
+% 0	1	1	0	0	-1	-1	0;
+% -1	-2	0	0	0	0	0	0;
+% 1	-3	0	0	0	0	0	0;
+% 0	-2	-1	0	0	0	0	0;
+% 1	-1	0	0	0	0	0	0];
+%stoichiometricMatrix = [-2 0 0 0  1; 1 -1 1 1 -1 ];
+[numberOfReactions, numberOfSpecies] = size(stoichiometricMatrix);
+kRates = ones(1,numberOfReactions);
+speciesConcentrations = [2 1 2 1 2];
+reducedMatrix = zeros(numberOfReactions,numberOfSpecies,numberOfSpecies);
+%{'a2*a5*k2 - 2*a1^2*k1'}
+for i = 1:numberOfReactions 
+    for j = 1:numberOfSpecies 
+            if stoichiometricMatrix(i,j)<0
+                 for k = 1:numberOfSpecies 
+                    if stoichiometricMatrix(i,k)<0
+                         reducedMatrix(i,k,j) = abs(stoichiometricMatrix(i,k));
+                    else
+                         reducedMatrix(i,k,j) = 0;
+                    end
+                 end
+             end
+            if stoichiometricMatrix(i,j)>0
+                for q = 1:numberOfSpecies 
+                    if stoichiometricMatrix(i,q)<0
+                        reducedMatrix(i,q,j) = abs(stoichiometricMatrix(i,q));
+                    else
+                        reducedMatrix(i,q,j) = 0;
+                    end
+                end
+            end
+            
+        
+    end
+end
+% *speciesConcentrations(2)^reducedMatrix(rxn,2,r)*speciesConcentrations(3)^reducedMatrix(rxn,3,r)...
+%                 *speciesConcentrations(4)^reducedMatrix(rxn,4,r)*speciesConcentrations(5)^reducedMatrix(rxn,5,r)*speciesConcentrations(6)^reducedMatrix(rxn,6,r));
+%             
+
+%kinetic balance equations
+speciesConcentrations = sym('n', [1 numberOfSpecies]);
+kRates = sym('k', [1 numberOfReactions]);
+eqntest = cell(numberOfSpecies,1);
+
+for r = 1:numberOfSpecies
+    for rxn = 1:numberOfReactions
+        term = char(stoichiometricMatrix(rxn,r)*kRates(rxn));
+        for species = 1:numberOfSpecies
+            m1 = char(speciesConcentrations(species)^reducedMatrix(rxn,species,r));
+            if ~strcmp(m1,'1')
+             term = strcat(term,['*' m1]);
+            end
+        end
+            if ~isempty(eqntest{r})&&~strcmp(term,'0')
+                eqntest{r} = strcat(eqntest{r},[' + ' term]);
+            elseif isempty(eqntest{r})&&~strcmp(term,'0')
+                eqntest{r} = term;
+            end
+    end
+end
+
+
+%mass balance eqn 
+
+
+%quasineutrality condition 
+%[Cl2 Cl Cl2+ Cl+ Cl- ne]
+charges = [0 0 1 1  -1 -1];
+quasineutralityCondition = [];
+for i=1:numberOfSpecies
+    q = char(speciesConcentrations(i)*charges(i))
+    if ~isempty(quasineutralityCondition)&&~strcmp(q,'0')
+        quasineutralityCondition = strcat(quasineutralityCondition,[' + ' q]);
+    elseif isempty(quasineutralityCondition)&&~strcmp(q,'0')
+        quasineutralityCondition = q;
+    end
+end
+
+%bohm velocity eqn
+
+
+
+
+%power balance equations
--- ReactionRate.m
+++ ReactionRate.m
@@ -1,14 +1,50 @@
 clear all
 close all
 
-stoichiometricMatrix = [-2 0 0 0 1; 1 -1 1 1 -1]
-zerosMatrix = [1 0 0 0 1; 1 1 1 1 1]
+
+
+stoichiometricMatrix = [-2 0 0 0 1; 1 -1 1 1 -1];
 numberOfReactions = 2;
 numberOfSpecies = 5;
-k = [1 1];
-speciesConcentrations = [ 2 1 1 1 1; 2 1 1 1 1];
-for j = 5:5%numberOfSpecies
-    %for i = 1:1%numberOfReactions
-        test = k*speciesConcentrations.^abs(stoichiometricMatrix).*stoichiometricMatrix;
-    %end
+kRates = [1 1];
+speciesConcentrations = [2 1 2 1 2];
+reducedMatrix = zeros(numberOfReactions,numberOfSpecies,numberOfSpecies);
+
+for i = 1:numberOfReactions 
+    for j = 1:numberOfSpecies 
+            if stoichiometricMatrix(i,j)<0
+                 for k = 1:numberOfSpecies 
+                    if stoichiometricMatrix(i,k)<0
+                         reducedMatrix(i,k,j) = abs(stoichiometricMatrix(i,k));
+                    else
+                         reducedMatrix(i,k,j) = 0;
+                    end
+                 end
+             end
+            if stoichiometricMatrix(i,j)>0
+                for q = 1:numberOfSpecies 
+                    if stoichiometricMatrix(i,q)<0
+                        reducedMatrix(i,q,j) = abs(stoichiometricMatrix(i,q));
+                    else
+                        reducedMatrix(i,q,j) = 0;
+                    end
+                end
+            end
+            
+        
+    end
 end
+
+speciesConcentrations = sym('a', [1 numberOfSpecies])
+kRates = sym('k', [1 numberOfReactions])
+for r = 1:numberOfSpecies
+            eqn{r} = char(stoichiometricMatrix(1,r)*kRates(1)*speciesConcentrations(1)^reducedMatrix(1,1,r)*speciesConcentrations(2)^reducedMatrix(1,2,r)*speciesConcentrations(3)^reducedMatrix(1,3,r)...
+                *speciesConcentrations(4)^reducedMatrix(1,4,r)*speciesConcentrations(5)^reducedMatrix(1,5,r)+ stoichiometricMatrix(2,r)*kRates(2)*speciesConcentrations(1)^reducedMatrix(2,1,r)...
+                *speciesConcentrations(2)^reducedMatrix(2,2,r)*speciesConcentrations(3)^reducedMatrix(2,3,r)*speciesConcentrations(4)^reducedMatrix(2,4,r)...
+                *speciesConcentrations(5)^reducedMatrix(2,5,r));
+end
+%create 3D matrix
+%how to write out all eqns
+%steady state eqn
+
+
